054775ffda0f1718d8c35fe9ef450e2e36490630
prevyne <prevyneoketch6@gmail.com>
2025-09-17 02:59:53 +0300

    feat: integrate Swagger for API documentation

--- job_board/urls.py
+++ job_board/urls.py
@@ -18,10 +18,26 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = "Job Board API",
+        default_version = 'v1',
+        description = "API Documentation for Project Nexus Job Board Platform",
+        contact = openapi.Contact(email="contact@jobboard.local"),
+        licence = openapi.License(name = "BSD Licence"),
+     ),
+    public = True,
+    permission_classes = (permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('jobs.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view, name='token_refresh'),
+    path('api/docs/', schema_view.with_ui('swagger', cache_timeout = 0), name = 'schema-swagger-ui'),
 ]
