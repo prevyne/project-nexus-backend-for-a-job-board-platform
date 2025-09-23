b590d4fd766d52bd98a5c2f0f87384b5de03156f
prevyne <prevyneoketch6@gmail.com>
2025-09-24 01:16:38 +0300

    Fix celery.py to use correct Django project name

--- job_board/celery.py
+++ job_board/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'job_board.settings')
 
-app = Celery('your_project')
+app = Celery('job_board')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
